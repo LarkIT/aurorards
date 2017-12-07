5dbf166fddb196709744117b3cfd0c2e8d187811
Andrew Alles <andrew@lark-it.com>
2017-12-07 15:31:44 -0700

    Committed right file this time

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,9 @@ output "aurora_cluster_id" {
 }
 
 output "aurora_cluster_instance" {
-  value = "${aws_rds_cluster_instance.aurora_cluster_instance_id}"
+  value = "${aws_rds_cluster_instance.aurora_cluster_instance.id}"
 }
 
+output "aurora_cluster_instance" {
+  value = "${aws_rds_cluster_instance.aurora_cluster_instance.id}"
+}
