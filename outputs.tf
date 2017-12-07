e678019a9280c7fb89704310a982e8c11516b401
Andrew Alles <andrew@lark-it.com>
2017-12-07 15:33:12 -0700

    Fixed outputs.tf, again

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,7 @@
 output "aurora_cluster_id" {
   value = "${aws_rds_cluster.aurora_cluster.id}"
-}
+}xs
 
-output "aurora_cluster_instance" {
-  value = "${aws_rds_cluster_instance.aurora_cluster_instance.id}"
-}
-
-output "aurora_cluster_instance" {
+output "aurora_cluster_instance_id" {
   value = "${aws_rds_cluster_instance.aurora_cluster_instance.id}"
 }
