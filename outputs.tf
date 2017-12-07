c21eb54532c943886f67040afc855cd36557eeff
Andrew Alles <andrew@lark-it.com>
2017-12-07 15:30:42 -0700

    Fixed id reference

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "aurora_cluster_id" {
 }
 
 output "aurora_cluster_instance" {
-  value = "${aws_rds_cluster_instance.aurora_cluster_instance.id}"
+  value = "${aws_rds_cluster_instance.aurora_cluster_instance_id}"
 }
 
