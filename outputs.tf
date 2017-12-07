d385940ab9128ae73635de1d3bb830caf8690161
Andrew Alles <andrew@lark-it.com>
2017-12-07 15:38:58 -0700

    Removed typo

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "aurora_cluster_id" {
   value = "${aws_rds_cluster.aurora_cluster.id}"
-}xs
+}
 
 output "aurora_cluster_instance_id" {
   value = "${aws_rds_cluster_instance.aurora_cluster_instance.id}"
