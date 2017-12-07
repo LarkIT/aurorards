87f7629c34f4e06323d6db73622207211378f4f5
Andrew Alles <andrew@lark-it.com>
2017-12-07 14:47:33 -0700

    Changed subnets to list

--- variables.tf
+++ variables.tf
@@ -16,12 +16,14 @@ variable "vpc_name" {
 
 variable "vpc_rds_subnet_ids" {
   description = "The ID's of the VPC subnets that the RDS cluster instances will be created in"
+  type = "list"
+  default = []
 }
 
 variable "vpc_rds_security_group_ids" {
-    description = "The IDs of the security groups that should be used for the RDS cluster instances"
-    type = "list"
-    default = []
+  description = "The IDs of the security groups that should be used for the RDS cluster instances"
+  type = "list"
+  default = []
 }
 
 variable "rds_master_username" {
